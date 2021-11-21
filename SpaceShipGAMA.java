2d878d67fb1973671e214acd4fba2f94ae2cf471
Sifis Lazanakis <46402835+sifislaz@users.noreply.github.com>
2021-11-21 13:00:22 +0200

    Add files via upload
    Correct 1st implementation.

---
+++ SpaceShipGAMA.java
@@ -0,0 +1,44 @@
+package spaceships;
+import MainPackage.MainClass;
+
+public class SpaceShipGAMA extends SpaceShip{
+	private int xPace = 30;
+	private int yPace = 30;
+	
+	
+	public SpaceShipGAMA(int spaceShipHeight, int spaceShipWidth) {
+		super(spaceShipHeight, spaceShipWidth);
+		this.spaceShipHeight = super.spaceShipHeight;
+		this.spaceShipWidth = super.spaceShipWidth;
+		super.xCoord = 0;
+		super.yCoord = MainClass.cosmosHeight - this.spaceShipHeight;
+	}
+	
+	
+	public int moveUP() {
+		yCoord -= yPace;
+		if(yCoord < 0) yCoord += yPace;  // if the spaceship is about to go out of bounds
+		return yCoord;
+	}
+	
+	
+	public int moveDOWN() {
+		yCoord += yPace;
+		if(yCoord + spaceShipHeight > MainClass.cosmosHeight) yCoord -= yPace;  // if the spaceship is about to go out of bounds
+		return yCoord;
+	}
+	
+	
+	public int moveLEFT() {
+		xCoord -= xPace;
+		if(xCoord < 0) xCoord += xPace;  // if the spaceship is about to go out of bounds
+		return xCoord;
+	}
+	
+	
+	public int moveRIGHT() {
+		xCoord += xPace;
+		if(xCoord + spaceShipWidth > MainClass.cosmosWidth) xCoord += xPace;  // if the spaceship is about to go out of bounds
+		return xCoord;
+	}
+}
